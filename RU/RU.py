9d3e2cef6b9f369b45e446a0435bfe42c28dc555
Laurynas Maciulis <laurynas.maciulis@vgtu.lt>
2018-06-08 18:14:14 +0300

    Optimization runs for cold case with 3 design var and COBYLA optimizer

--- RU/RU.py
+++ RU/RU.py
@@ -25,12 +25,12 @@ file.close()
 
 class RU_cold(ExternalCode):
     def setup(self):
-        #self.add_input('batH', val=0.1)
-        #self.add_input('propH', val=0.1)
+        self.add_input('batH', val=0.1)
+        self.add_input('propH', val=0.1)
         self.add_input('eps-Y', val=0.5)
         self.add_output('tBat', val=0.0)
-        #self.add_output('T_prop', val=0.0)
-        #self.add_output('T_main', val=0.0)
+        self.add_output('tProp', val=0.0)
+        self.add_output('tMain', val=0.0)
 
         self.input_file = 'RU_cold.d'
         self.output_file = 'RU_cold.out'
@@ -49,7 +49,12 @@ class RU_cold(ExternalCode):
 
     def compute(self, inputs, outputs):
         eps100 = inputs['eps-Y']
+        qi300 = inputs['propH']
+        qi10 = inputs['batH']
         eps100 = '{0},'.format(float(eps100)) 
+        qi300 = '{0}'.format(float(qi300)) 
+        qi10 = '{0}'.format(float(qi10))
+        qi20 = qi10 
 
         # generate the input file for esatan thermal analysis
         generator = InputFileGenerator()
@@ -57,6 +62,10 @@ class RU_cold(ExternalCode):
         generator.set_generated_file('RU_cold.d')
         generator.mark_anchor("$NODES")
         generator.transfer_var(eps100, 53, 9)
+        generator.mark_anchor("$INITIAL")
+        generator.transfer_var(qi300, 7, 3)
+        generator.transfer_var(qi10, 10, 3)
+        generator.transfer_var(qi20, 13, 3)
         generator.generate()
 
         # the parent compute function actually runs the external code
@@ -67,43 +76,53 @@ class RU_cold(ExternalCode):
         parser.set_file('RU_cold.out')
         parser.mark_anchor("+RU_COLD")
         tBat = parser.transfer_var(7, 3)
+        tMain = parser.transfer_var(34, 3)
+        tProp = parser.transfer_var(35, 3)
 
         outputs['tBat'] = tBat
+        outputs['tMain'] = tMain
+        outputs['tProp'] = tProp
 
 prob = Problem()
 model = prob.model
 
-# create and connect inputs
-indeps = model.add_subsystem('indeps', IndepVarComp())
-""" indeps.add_output('batH', 0.14) 
-indeps.add_output('tileH', 0.1) """ 
-indeps.add_output('eps-Y', 0.02) 
-model.add_subsystem('esatan', RU_cold())
+# create and connect inputs and outputs
+indeps = model.add_subsystem('indeps', IndepVarComp(), promotes=['*'])
+indeps.add_output('batH', 0.01) 
+indeps.add_output('propH', 0.01) 
+indeps.add_output('eps-Y', 0.5) 
 
-#objective function is radiator area A
-#model.add_subsystem('obj', ExecComp('A = length * width'))
+model.add_subsystem('esatan', RU_cold(), promotes=['*'])
 
-#model.connect('indeps.RadLen', ['esatan.RadLen', 'obj.length'])
-model.connect('indeps.eps-Y', 'esatan.eps-Y')
+#objective function is total heater power
+model.add_subsystem('obj', ExecComp('Power = batH + propH'), promotes=['*'])
 
-""" # find optimal solution with SciPy optimize
+# find optimal solution with SciPy optimize
 prob.driver = ScipyOptimizeDriver()
-#prob.driver.options['optimizer'] = 'COBYLA'
-#prob.driver.options['disp'] = False
-prob.driver.options['optimizer'] = 'SLSQP'
-prob.driver.opt_settings = {'eps': 1.0e-12, 'ftol':1e-04}
+prob.driver.options['optimizer'] = 'COBYLA'
+prob.driver.options['disp'] = False
+prob.driver.opt_settings = {'maxiter': 100, 'tol':1e-02, 'catol':1e-01}
 
-prob.model.add_design_var('indeps.RadLen', lower=0.1, upper=0.5)
-prob.model.add_objective('obj.A')
+prob.model.add_design_var('batH', lower=0.0, upper=1.0)
+prob.model.add_design_var('propH', lower=0.0, upper=1.0)
+prob.model.add_design_var('eps-Y', lower=0.02, upper=1.0)
+
+prob.model.add_objective('Power')
+
 #constraint for payload electronics max temperature
-prob.model.add_constraint('esatan.T_max', upper=20) """
+prob.model.add_constraint('tBat', lower=0.0)
+prob.model.add_constraint('tProp', lower=-10.0)
+prob.model.add_constraint('tMain', lower=-40.0)
 
 # run the ExternalCode Component
 prob.setup(check=True, mode='fwd')
-prob.run_model()
-#prob.run_driver()
+#prob.run_model()
+prob.run_driver()
 
 # print the output temperature, minimum radiator length and area
-print(prob['esatan.tBat'])
+print(prob['Power'])
+print(prob['tBat'])
+print(prob['tProp'])
+print(prob['tMain'])
 #print(prob['indeps.RadLen'])
 #print(prob['obj.A'])
