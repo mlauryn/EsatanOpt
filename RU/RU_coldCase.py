8682e9ba8a031a5db888add94f6a37a48590fb64
Laurynas Maciulis <laurynas.maciulis@vgtu.lt>
2018-06-10 13:41:07 +0300

    Separated single case from 2 case analysis, corrected template file

--- RU/RU_coldCase.py
+++ RU/RU_coldCase.py
@@ -1,5 +1,5 @@
-#Python script for optimization of MAT remote unit thermal model
-import os
+#Python script for optimization of MAT remote unit thermal model @cold analysis case
+import os, time
 from openmdao.api import Problem, Group, IndepVarComp, ExternalCode, ScipyOptimizeDriver, SimpleGADriver, ExecComp
 from openmdao.utils.file_wrap import InputFileGenerator, FileParser
 
@@ -21,22 +21,6 @@ file.close()
 
 file = open("RU_cold.bat", "w")
 file.write('''esrde<RU_cold.ere''')
-file.close()
-
-file = open("RU_hot.ere", 'w')
-file.write('''BEGIN_MODEL RU_hot
-ANALYSIS_CASE optimization;'
-DEFINE_ANALYSIS_CASE (
-    analysis_case = optimization,
-    working_directory = "{path}");'
-RUN_ANALYSIS(
-    analysis_case = optimization,
-    file = "RU_hot.d");'
-END_MODEL'''.format(path=os.getcwd()))
-file.close()
-
-file = open("RU_hot.bat", "w")
-file.write('''esrde<RU_hot.ere''')
 file.close()
 
 class RU_cold(ExternalCode):
@@ -80,7 +64,7 @@ class RU_cold(ExternalCode):
         self.options['command'] = [
             'RU_cold.bat']
         # this external code does not provide derivatives, use finite difference
-        #self.declare_partials(of='*', wrt='*', method='fd')
+        self.declare_partials(of='*', wrt='*', method='fd')
 
     def compute(self, inputs, outputs):
         alp = inputs['alp']
@@ -186,142 +170,6 @@ class RU_cold(ExternalCode):
         outputs['tProp'] = tProp
         outputs['tTether'] = tTether
 
-class RU_hot(ExternalCode):
-    def setup(self):
-        self.add_input('eps', val=0.4)
-        self.add_input('alp', val=0.4)
-        self.add_input('GlBat1', val=0.4)
-        self.add_input('GlBat2', val=0.4)
-        self.add_input('GlMain', val=0.4)
-        self.add_input('GlProp', val=0.4)
-        self.add_input('GlTether', val=0.4)
-        self.add_input('ci1', val=0.4)
-        self.add_input('ci2', val=0.4)
-        self.add_input('ci3', val=0.4)
-        self.add_input('ci4', val=0.4)
-        self.add_input('ci5', val=0.4)
-        self.add_input('ci6', val=0.4)
-        self.add_input('ci7', val=0.4)
-        self.add_input('ci8', val=0.4)
-        self.add_input('ci9', val=0.4)
-        self.add_input('ci10', val=0.4)
-        self.add_input('ci11', val=0.4)
-        self.add_input('ci12', val=0.4)
-        self.add_output('tBat', val=0.0)
-        self.add_output('tProp', val=0.0)
-        self.add_output('tMain', val=0.0)
-        self.add_output('tTether', val=0.0)
-
-        self.input_file = 'RU_hot.d'
-        self.output_file = 'RU_hot.out'
-
-        # providing these is optional;' the component will verify that any input
-        # files exist before execution and that the output files exist after.
-        #self.options['external_input_files'] = [self.input_file,]
-        #self.options['external_output_files'] = [self.output_file,]
-        #self.options['poll_delay'] = 10.0
-        self.options['timeout'] = 10.0
-        self.options['fail_hard'] = False
-        self.options['command'] = [
-            'RU_hot.bat']
-        # this external code does not provide derivatives, use finite difference
-        #self.declare_partials(of='*', wrt='*', method='fd')
-
-    def compute(self, inputs, outputs):
-        alp = inputs['alp']
-        eps = inputs['eps']
-        GL11_200 = inputs['GlBat1']
-        GL21_200 = inputs['GlBat2']
-        GL107_200 = inputs['GlMain']
-        GL111_300 = inputs['GlProp']
-        GL103_400 = inputs['GlTether']
-        GL107_111 = inputs['ci1']
-        GL107_109 = inputs['ci2']
-        GL103_109 = inputs['ci3']
-        GL101_109 = inputs['ci4']
-        GL109_111 = inputs['ci5']
-        GL101_103 = inputs['ci6']
-        GL103_111 = inputs['ci7']
-        GL101_107 = inputs['ci8']
-        GL103_105 = inputs['ci9']
-        GL101_105 = inputs['ci10']
-        GL105_111 = inputs['ci11']
-        GL105_107 = inputs['ci12']
-
-        alp = '{0},'.format(float(alp)) 
-        eps = '{0},'.format(float(eps)) 
-        GL11_200 = '{0};'.format(float(GL11_200))
-        GL21_200 = '{0};'.format(float(GL21_200))
-        GL107_200 = '{0};'.format(float(GL107_200))
-        GL111_300 = '{0};'.format(float(GL111_300))
-        GL103_400 = '{0};'.format(float(GL103_400))
-        GL107_111 = '{0};'.format(float(GL107_111))
-        GL107_109 = '{0};'.format(float(GL107_109))
-        GL103_109 = '{0};'.format(float(GL103_109))
-        GL101_109 = '{0};'.format(float(GL101_109))
-        GL109_111 = '{0};'.format(float(GL109_111))
-        GL101_103 = '{0};'.format(float(GL101_103))
-        GL103_111 = '{0};'.format(float(GL103_111))
-        GL101_107 = '{0};'.format(float(GL101_107))
-        GL103_105 = '{0};'.format(float(GL103_105))
-        GL101_105 = '{0};'.format(float(GL101_105))
-        GL105_111 = '{0};'.format(float(GL105_111))
-        GL105_107 = '{0};'.format(float(GL105_107))
-
-        # generate the input file for RU_hot thermal analysis
-        generator = InputFileGenerator()
-        generator.set_template_file('RU_hot_template.txt')
-        generator.set_generated_file('RU_hot.d')
-        generator.mark_anchor("$NODES")
-        generator.transfer_var(alp, 53, 6)
-        generator.transfer_var(eps, 53, 9)
-        generator.transfer_var(alp, 61, 6)
-        generator.transfer_var(eps, 61, 9)
-        generator.transfer_var(alp, 69, 6)
-        generator.transfer_var(eps, 69, 9)
-        generator.transfer_var(alp, 77, 6)
-        generator.transfer_var(eps, 77, 9)
-        generator.transfer_var(alp, 85, 6)
-        generator.transfer_var(eps, 85, 9)
-        generator.transfer_var(alp, 93, 6)
-        generator.transfer_var(eps, 93, 9)
-        generator.mark_anchor("Generated conductors")
-        generator.transfer_var(GL11_200, 1, 3)  
-        generator.transfer_var(GL21_200, 2, 3)
-        generator.transfer_var(GL107_200, 3, 3)
-        generator.transfer_var(GL111_300, 4, 3)
-        generator.transfer_var(GL107_111, 5, 3)
-        generator.transfer_var(GL107_109, 6, 3)
-        generator.transfer_var(GL103_109, 7, 3)
-        generator.transfer_var(GL101_109, 8, 3)
-        generator.transfer_var(GL109_111, 9, 3)
-        generator.transfer_var(GL101_103, 10, 3)
-        generator.transfer_var(GL103_111, 11, 3)
-        generator.transfer_var(GL101_107, 12, 3)
-        generator.transfer_var(GL103_105, 13, 3)
-        generator.transfer_var(GL101_105, 14, 3)
-        generator.transfer_var(GL105_111, 15, 3)
-        generator.transfer_var(GL105_107, 16, 3)
-        generator.transfer_var(GL103_400, 45, 3)
-        generator.generate()
-
-        # the parent compute function actually runs the external code
-        super(RU_hot, self).compute(inputs, outputs)
-
-        # parse the output file from the external code and set the value of T_max
-        parser = FileParser()
-        parser.set_file('RU_hot.out')
-        parser.mark_anchor("+RU_HOT")
-        tBat = parser.transfer_var(7, 3)
-        tMain = parser.transfer_var(34, 3)
-        tProp = parser.transfer_var(35, 3)
-        tTether = parser.transfer_var(36, 3)
-
-        outputs['tBat'] = tBat
-        outputs['tMain'] = tMain
-        outputs['tProp'] = tProp
-        outputs['tTether'] = tTether
-
 prob = Problem()
 model = prob.model
 
@@ -336,7 +184,7 @@ indeps.add_output('GlBat2', val=0.4)
 indeps.add_output('GlMain', val=0.04)
 indeps.add_output('GlProp', val=0.04)
 indeps.add_output('GlTether', val=0.04)
-""" indeps.add_output('ci1', val=0.4)
+indeps.add_output('ci1', val=0.4)
 indeps.add_output('ci2', val=0.4)
 indeps.add_output('ci3', val=0.4)
 indeps.add_output('ci4', val=0.4)
@@ -347,21 +195,25 @@ indeps.add_output('ci8', val=0.4)
 indeps.add_output('ci9', val=0.4)
 indeps.add_output('ci10', val=0.4)
 indeps.add_output('ci11', val=0.4)
-indeps.add_output('ci12', val=0.4) """ 
+indeps.add_output('ci12', val=0.4) 
 
 
 model.add_subsystem('RU_cold', RU_cold(), promotes_inputs=['*'], promotes_outputs=[('tBat','tBat_c'), 
                     ('tMain','tMain_c'), ('tProp','tProp_c')])
-model.add_subsystem('RU_hot', RU_hot(), promotes_inputs=['*'], promotes_outputs=[('tBat','tBat_h'), 
-                    ('tMain','tMain_h'), ('tProp','tProp_h'), ('tTether','tTether_h')])
 
 #objective function is temperature difference btw hot and cold cases
-model.add_subsystem('obj', ExecComp('deltaT = tBat_h+tProp_h+tMain_h+tTether_h-tBat_c-tProp_c-tMain_c'), 
+model.add_subsystem('obj', ExecComp('sumT_c = -tBat_c-tProp_c-tMain_c'), 
                     promotes=['*'])
 
+
+""" prob.driver = ScipyOptimizeDriver()
+prob.driver.options['optimizer']='SLSQP'
+prob.driver.options['disp'] = True
+prob.driver.opt_settings = {'eps': 1.0e-6, 'ftol':1e-04,} """
 # find optimal solution with simple GA driver
 prob.driver = SimpleGADriver()
-prob.driver.options['bits'] = {'eps': 5, 'alp': 5, 'GlBat1': 3, 'GlBat2':3, 'GlMain':5, 'GlProp':5, 'GlTether':5}
+prob.driver.options['bits'] = {'eps': 5, 'alp': 5, 'GlBat1': 3, 'GlBat2':3, 'GlMain':5, 'GlProp':5, 'GlTether':5,
+    'ci1':5, 'ci2':5, 'ci3':5, 'ci4':5, 'ci5':5, 'ci6':5, 'ci7':5, 'ci8':5, 'ci9':5, 'ci10':5, 'ci11':5, 'ci12':5}
 prob.driver.options['max_gen'] = 5
 #prob.driver.options['run_parallel'] = 'true'
 prob.driver.options['debug_print'] = ['desvars']
@@ -373,7 +225,7 @@ prob.model.add_design_var('GlBat2', lower = 0.4, upper=26.0)
 prob.model.add_design_var('GlMain', lower = 0.004, upper=1.0)
 prob.model.add_design_var('GlProp', lower = 0.004, upper=1.0)
 prob.model.add_design_var('GlTether', lower = 0.004, upper=1.0)
-""" prob.model.add_design_var('ci1', lower = 0.013, upper=0.072)
+prob.model.add_design_var('ci1', lower = 0.013, upper=0.072)
 prob.model.add_design_var('ci2', lower = 0.015, upper=0.084)
 prob.model.add_design_var('ci3', lower = 0.015, upper=0.084)
 prob.model.add_design_var('ci4', lower = 0.008, upper=0.026)
@@ -384,33 +236,38 @@ prob.model.add_design_var('ci8', lower = 0.013, upper=0.072)
 prob.model.add_design_var('ci9', lower = 0.015, upper=0.084)
 prob.model.add_design_var('ci10', lower = 0.008, upper=0.026)
 prob.model.add_design_var('ci11', lower = 0.008, upper=0.026)
-prob.model.add_design_var('ci12', lower = 0.015, upper=0.084) """
+prob.model.add_design_var('ci12', lower = 0.015, upper=0.084)
 
-prob.model.add_objective('deltaT')
+prob.model.add_objective('sumT_c')
 
 #constraint for  temperatures
-""" prob.model.add_constraint('RU_cold.tBat', lower=0.0, upper = 45.0)
-prob.model.add_constraint('RU_cold.tProp', lower=-10.0, upper = 80.0)
-prob.model.add_constraint('RU_cold.tMain', lower=-40.0, upper = 85.0)
-prob.model.add_constraint('RU_hot.tBat', lower=0.0, upper = 45.0)
-prob.model.add_constraint('RU_hot.tProp', lower=-10.0, upper = 80.0)
-prob.model.add_constraint('RU_hot.tMain', lower=-40.0, upper = 85.0)
-prob.model.add_constraint('RU_hot.tTether', lower=-40.0, upper = 50.0) """
+""" prob.model.add_constraint('tBat_c', lower=0.0, upper = 45.0)
+prob.model.add_constraint('tProp_c', lower=-10.0, upper = 80.0)
+prob.model.add_constraint('tMain_c', lower=-40.0, upper = 85.0) """
 
-# run the ExternalCode Component
-prob.setup(check=True)
-#prob.run_model()
-prob.run_driver()
+# run the ExternalCode Component once
+prob.setup(check=True, mode='fwd')
+prob.run_model()
 
-# print the output temperature, minimum radiator length and area
-print(prob['Power'])
-print(prob['tBat_c'])
-print(prob['tProp_c'])
-print(prob['tMain_c'])
-print(prob['tTether_c'])
-print(prob['tBat_h'])
-print(prob['tProp_h'])
-print(prob['tMain_h'])
-print(prob['tTether_h'])
+#Record initial temperatures
+tBat_1 =  prob['tBat_c']
+tProp_1 =  prob['tProp_c']
+tMain_1 =  prob['tMain_c']
+
+#Run optimization
+tStart = time.time()
+#prob.run_driver()
+
+#Record final temperatures
+tBat_2 =  prob['tBat_c']
+tProp_2 =  prob['tProp_c']
+tMain_2 =  prob['tMain_c']
+
+print("Temperatures before optimization:, tBat_1={}, tProp_1={}, tMain_1={}".format(tBat_1, tProp_1, tMain_1)) 
+print("Temperatures after optimization:, tBat_2={}, tProp_2={}, tMain_2={}".format(tBat_2, tProp_2, tMain_2))
+print("Final design variables: eps={}, alp={}, GlBat1={}, GlBat2={}, GlMain={}, GlProp={}, GlTether={}, ci1={}, ci2={}, ci3={}, ci4={}, ci5={}, ci6={}, ci7={}, ci8={}, ci9={}, ci10={}, ci11={}, ci12={}".format (prob['eps'], prob['alp'], prob['GlBat1'], prob['GlBat2'], prob['GlMain'], prob['GlProp'], prob['GlTether'],
+prob['ci1'], prob['ci2'], prob['ci3'], prob['ci4'], prob['ci5'], prob['ci6'], prob['ci7'], prob['ci8'], prob['ci9'], prob['ci10'], prob['ci11'], prob['ci12']))
+print("Optimization run time in minutes:", (time.time()-tStart)/60) 
+
 #print(prob['indeps.RadLen'])
 #print(prob['obj.A'])
