6feac077ba3a78ff92c81c96a998d18dd7141c55
Laurynas Maciulis <laurynas.maciulis@vgtu.lt>
2020-03-02 19:07:05 +0200

    RU models updated for MDO

--- RU/RU_cold_esatan.py
+++ RU/RU_cold_esatan.py
@@ -25,31 +25,33 @@ file.close()
 
 class RU_cold(om.ExternalCode):
     def setup(self):
-        self.add_input('batH', val=0.1)
-        self.add_input('propH', val=0.1)
+        self.add_input('length', val=0.1)
+        self.add_input('P_ht', val=0.4)
+        self.add_input('r_bat', val=0.5)
         self.add_input('eps', val=0.02)
         self.add_input('alp', val=0.19)
-        self.add_input('GlBat1', val=0.4)
-        self.add_input('GlBat2', val=0.4)
         self.add_input('GlMain', val=0.4)
         self.add_input('GlProp', val=0.4)
         self.add_input('GlTether', val=0.4)
-        self.add_input('ci1', val=0.4)
-        self.add_input('ci2', val=0.4)
-        self.add_input('ci3', val=0.4)
-        self.add_input('ci4', val=0.4)
-        self.add_input('ci5', val=0.4)
-        self.add_input('ci6', val=0.4)
-        self.add_input('ci7', val=0.4)
-        self.add_input('ci8', val=0.4)
-        self.add_input('ci9', val=0.4)
-        self.add_input('ci10', val=0.4)
-        self.add_input('ci11', val=0.4)
-        self.add_input('ci12', val=0.4)
+        self.add_input('GlPanel', val=0.4)
+        self.add_input('ci1', val=1.0)
+        self.add_input('ci2', val=1.0)
+        self.add_input('ci3', val=1.0)
+        self.add_input('ci4', val=1.0)
+        self.add_input('ci5', val=1.0)
+        self.add_input('ci6', val=1.0)
+        self.add_input('ci7', val=1.0)
+        self.add_input('ci8', val=1.0)
+        self.add_input('ci9', val=1.0)
+        self.add_input('ci10', val=1.0)
+        self.add_input('ci11', val=1.0)
+        self.add_input('ci12', val=1.0)
         self.add_output('tBat', val=0.0)
         self.add_output('tProp', val=0.0)
         self.add_output('tMain', val=0.0)
         self.add_output('tTether', val=0.0)
+        self.add_output('tBPanel', val=0.0)
+        self.add_output('tDPanel', val=0.0)
 
         self.input_file = 'RU_cold.d'
         self.output_file = 'RU_cold.out'
@@ -67,13 +69,13 @@ class RU_cold(om.ExternalCode):
         #self.declare_partials(of='*', wrt='*', method='fd')
 
     def compute(self, inputs, outputs):
+        length = inputs['length']
         alp = inputs['alp']
         eps = inputs['eps']
-        GL11_200 = inputs['GlBat1']
-        GL21_200 = inputs['GlBat2']
         GL107_200 = inputs['GlMain']
         GL111_300 = inputs['GlProp']
         GL103_400 = inputs['GlTether']
+        GL102_601 = inputs['GlPanel']
         GL107_111 = inputs['ci1']
         GL107_109 = inputs['ci2']
         GL103_109 = inputs['ci3']
@@ -86,16 +88,18 @@ class RU_cold(om.ExternalCode):
         GL101_105 = inputs['ci10']
         GL105_111 = inputs['ci11']
         GL105_107 = inputs['ci12']
-        qi300 = inputs['propH']
-        qi10 = inputs['batH']
+        P_ht = inputs['P_ht']
+        r_bat = inputs['r_bat']
 
+        length = '{0};'.format(float(length))
+        P_ht = '{0};'.format(float(P_ht)) 
+        r_bat = '{0};'.format(float(r_bat))
         alp = '{0},'.format(float(alp)) 
         eps = '{0},'.format(float(eps)) 
-        GL11_200 = '{0};'.format(float(GL11_200))
-        GL21_200 = '{0};'.format(float(GL21_200))
         GL107_200 = '{0};'.format(float(GL107_200))
         GL111_300 = '{0};'.format(float(GL111_300))
         GL103_400 = '{0};'.format(float(GL103_400))
+        GL102_601 = '{0};'.format(float(GL102_601))
         GL107_111 = '{0};'.format(float(GL107_111))
         GL107_109 = '{0};'.format(float(GL107_109))
         GL103_109 = '{0};'.format(float(GL103_109))
@@ -108,14 +112,16 @@ class RU_cold(om.ExternalCode):
         GL101_105 = '{0};'.format(float(GL101_105))
         GL105_111 = '{0};'.format(float(GL105_111))
         GL105_107 = '{0};'.format(float(GL105_107))  
-        qi300 = '{0}'.format(float(qi300)) 
-        qi10 = '{0}'.format(float(qi10))
-        qi20 = qi10
+
 
         # generate the input file for RU_cold thermal analysis
         generator = InputFileGenerator()
         generator.set_template_file('RU_cold_template.txt')
         generator.set_generated_file('RU_cold.d')
+        generator.mark_anchor("$LOCALS")
+        generator.transfer_var(length, 21, 3)
+        generator.transfer_var(P_ht, 25, 3)
+        generator.transfer_var(r_bat, 27, 3)
         generator.mark_anchor("$NODES")
         generator.transfer_var(alp, 53, 6)
         generator.transfer_var(eps, 53, 9)
@@ -130,8 +136,6 @@ class RU_cold(om.ExternalCode):
         generator.transfer_var(alp, 93, 6)
         generator.transfer_var(eps, 93, 9)
         generator.mark_anchor("Generated conductors")
-        generator.transfer_var(GL11_200, 1, 3)  
-        generator.transfer_var(GL21_200, 2, 3)
         generator.transfer_var(GL107_200, 3, 3)
         generator.transfer_var(GL111_300, 4, 3)
         generator.transfer_var(GL107_111, 5, 3)
@@ -146,11 +150,8 @@ class RU_cold(om.ExternalCode):
         generator.transfer_var(GL101_105, 14, 3)
         generator.transfer_var(GL105_111, 15, 3)
         generator.transfer_var(GL105_107, 16, 3)
-        generator.transfer_var(GL103_400, 44, 3)
-        generator.mark_anchor("$INITIAL")
-        generator.transfer_var(qi300, 7, 3)
-        generator.transfer_var(qi10, 10, 3)
-        generator.transfer_var(qi20, 13, 3)
+        generator.transfer_var(GL102_601, 17, 3)
+        generator.transfer_var(GL103_400, 45, 3)
         generator.generate()
 
         # the parent compute function actually runs the external code
@@ -164,8 +165,56 @@ class RU_cold(om.ExternalCode):
         tMain = parser.transfer_var(34, 3)
         tProp = parser.transfer_var(35, 3)
         tTether = parser.transfer_var(36, 3)
+        tBPanel = parser.transfer_var(41, 3)
+        tDPanel = parser.transfer_var(43, 3)
 
         outputs['tBat'] = tBat
         outputs['tMain'] = tMain
         outputs['tProp'] = tProp
         outputs['tTether'] = tTether
+        outputs['tBPanel'] = tBPanel
+        outputs['tDPanel'] = tDPanel
+
+
+if __name__ == "__main__":
+    prob = om.Problem()
+    model = prob.model
+
+    # create and connect inputs and outputs
+    indeps = model.add_subsystem('indeps', om.IndepVarComp(), promotes=['*'])
+    indeps.add_output('length', val=0.1)
+    indeps.add_output('P_ht', val=0.5)
+    indeps.add_output('r_bat', val=0.5)
+    indeps.add_output('eps', val=0.1)
+    indeps.add_output('alp', val=0.4)
+    indeps.add_output('GlMain', val=0.004)
+    indeps.add_output('GlProp', val=0.004)
+    indeps.add_output('GlTether', val=0.004)
+    indeps.add_output('GlPanel', val=1.0)
+    indeps.add_output('ci1', val=1.0)
+    indeps.add_output('ci2', val=1.0)
+    indeps.add_output('ci3', val=1.0)
+    indeps.add_output('ci4', val=1.0)
+    indeps.add_output('ci5', val=1.0)
+    indeps.add_output('ci6', val=1.0)
+    indeps.add_output('ci7', val=1.0)
+    indeps.add_output('ci8', val=1.0)
+    indeps.add_output('ci9', val=1.0)
+    indeps.add_output('ci10', val=1.0)
+    indeps.add_output('ci11', val=1.0)
+    indeps.add_output('ci12', val=1.0) 
+
+
+    model.add_subsystem('RU_cold', RU_cold(), promotes_inputs=['*'], promotes_outputs=[('tBat','tBat_c'), 
+                        ('tMain','tMain_c'), ('tProp','tProp_c'), ('tBPanel', 'tBPanel_c'), ('tDPanel', 'tDPanel_c')])
+
+    #run the ExternalCode Component once and print outputs
+    prob.setup(check=True)
+    prob.run_model()
+
+    tBat =  prob['tBat_c']
+    tProp =  prob['tProp_c']
+    tMain =  prob['tMain_c']
+    tBPanel = prob['tBPanel_c']
+    tDPanel = prob['tDPanel_c']
+    print("Temperatures:, tBat={}, tProp={}, tMain={}, tBPanel={}, tDPanel={}".format(tBat, tProp, tMain, tBPanel, tDPanel))
--- RU/RU_hot_esatan.py
+++ RU/RU_hot_esatan.py
@@ -25,12 +25,14 @@ file.close()
 
 class RU_hot(om.ExternalCode):
     def setup(self):
+        self.add_input('length', val=0.1)
+        self.add_input('P_un', val=0.4)
         self.add_input('eps', val=0.4)
         self.add_input('alp', val=0.4)
-        self.add_input('GlBat', val=0.4)
         self.add_input('GlMain', val=0.4)
         self.add_input('GlProp', val=0.4)
         self.add_input('GlTether', val=0.4)
+        self.add_input('GlPanel', val=0.4)
         self.add_input('ci1', val=0.4)
         self.add_input('ci2', val=0.4)
         self.add_input('ci3', val=0.4)
@@ -47,6 +49,8 @@ class RU_hot(om.ExternalCode):
         self.add_output('tProp', val=0.0)
         self.add_output('tMain', val=0.0)
         self.add_output('tTether', val=0.0)
+        self.add_output('tBPanel', val=0.0)
+        self.add_output('tDPanel', val=0.0)
 
         self.input_file = 'RU_hot.d'
         self.output_file = 'RU_hot.out'
@@ -64,13 +68,13 @@ class RU_hot(om.ExternalCode):
         #self.declare_partials(of='*', wrt='*', method='fd')
 
     def compute(self, inputs, outputs):
+        length = inputs['length']
         alp = inputs['alp']
         eps = inputs['eps']
-        GL11_200 = inputs['GlBat']
-        GL21_200 = inputs['GlBat']
         GL107_200 = inputs['GlMain']
         GL111_300 = inputs['GlProp']
         GL103_400 = inputs['GlTether']
+        GL102_601 = inputs['GlPanel']
         GL107_111 = inputs['ci1']
         GL107_109 = inputs['ci2']
         GL103_109 = inputs['ci3']
@@ -83,14 +87,16 @@ class RU_hot(om.ExternalCode):
         GL101_105 = inputs['ci10']
         GL105_111 = inputs['ci11']
         GL105_107 = inputs['ci12']
+        P_un = inputs['P_un']
 
+        length = '{0};'.format(float(length))
+        P_un = '{0};'.format(float(P_un))
         alp = '{0},'.format(float(alp)) 
         eps = '{0},'.format(float(eps)) 
-        GL11_200 = '{0};'.format(float(GL11_200))
-        GL21_200 = '{0};'.format(float(GL21_200))
         GL107_200 = '{0};'.format(float(GL107_200))
         GL111_300 = '{0};'.format(float(GL111_300))
         GL103_400 = '{0};'.format(float(GL103_400))
+        GL102_601 = '{0};'.format(float(GL102_601))
         GL107_111 = '{0};'.format(float(GL107_111))
         GL107_109 = '{0};'.format(float(GL107_109))
         GL103_109 = '{0};'.format(float(GL103_109))
@@ -108,6 +114,9 @@ class RU_hot(om.ExternalCode):
         generator = InputFileGenerator()
         generator.set_template_file('RU_hot_template.txt')
         generator.set_generated_file('RU_hot.d')
+        generator.mark_anchor("$LOCALS")
+        generator.transfer_var(length, 21, 3)
+        generator.transfer_var(P_un, 25, 3)
         generator.mark_anchor("$NODES")
         generator.transfer_var(alp, 53, 6)
         generator.transfer_var(eps, 53, 9)
@@ -122,8 +131,6 @@ class RU_hot(om.ExternalCode):
         generator.transfer_var(alp, 93, 6)
         generator.transfer_var(eps, 93, 9)
         generator.mark_anchor("Generated conductors")
-        generator.transfer_var(GL11_200, 1, 3)  
-        generator.transfer_var(GL21_200, 2, 3)
         generator.transfer_var(GL107_200, 3, 3)
         generator.transfer_var(GL111_300, 4, 3)
         generator.transfer_var(GL107_111, 5, 3)
@@ -138,6 +145,7 @@ class RU_hot(om.ExternalCode):
         generator.transfer_var(GL101_105, 14, 3)
         generator.transfer_var(GL105_111, 15, 3)
         generator.transfer_var(GL105_107, 16, 3)
+        generator.transfer_var(GL102_601, 17, 3)
         generator.transfer_var(GL103_400, 45, 3)
         generator.generate()
 
@@ -152,8 +160,53 @@ class RU_hot(om.ExternalCode):
         tMain = parser.transfer_var(34, 3)
         tProp = parser.transfer_var(35, 3)
         tTether = parser.transfer_var(36, 3)
+        tBPanel = parser.transfer_var(41, 3)
+        tDPanel = parser.transfer_var(43, 3)
 
         outputs['tBat'] = tBat
         outputs['tMain'] = tMain
         outputs['tProp'] = tProp
         outputs['tTether'] = tTether
+        outputs['tBPanel'] = tBPanel
+        outputs['tDPanel'] = tDPanel
+if __name__ == "__main__":
+    prob = om.Problem()
+    model = prob.model
+
+    # create and connect inputs and outputs
+    indeps = model.add_subsystem('indeps', om.IndepVarComp(), promotes=['*'])
+    indeps.add_output('length', val=0.2)
+    indeps.add_output('P_un', val=1.0)
+    indeps.add_output('eps', val=0.1)
+    indeps.add_output('alp', val=0.4)
+    indeps.add_output('GlMain', val=0.004)
+    indeps.add_output('GlProp', val=0.004)
+    indeps.add_output('GlTether', val=0.004)
+    indeps.add_output('GlPanel', val=0.004)
+    indeps.add_output('ci1', val=1.0)
+    indeps.add_output('ci2', val=1.0)
+    indeps.add_output('ci3', val=1.0)
+    indeps.add_output('ci4', val=1.0)
+    indeps.add_output('ci5', val=1.0)
+    indeps.add_output('ci6', val=1.0)
+    indeps.add_output('ci7', val=1.0)
+    indeps.add_output('ci8', val=1.0)
+    indeps.add_output('ci9', val=1.0)
+    indeps.add_output('ci10', val=1.0)
+    indeps.add_output('ci11', val=1.0)
+    indeps.add_output('ci12', val=1.0) 
+
+
+    model.add_subsystem('RU_hot', RU_hot(), promotes_inputs=['*'], promotes_outputs=[('tBat','tBat_h'), 
+                        ('tMain','tMain_h'), ('tProp','tProp_h'), ('tBPanel', 'tBPanel_h'), ('tDPanel', 'tDPanel_h')])
+
+    #run the ExternalCode Component once and print outputs
+    prob.setup(check=True)
+    prob.run_model()
+
+    tBat =  prob['tBat_h']
+    tProp =  prob['tProp_h']
+    tMain =  prob['tMain_h']
+    tBPanel = prob['tBPanel_h']
+    tDPanel = prob['tDPanel_h']    
+    print("Temperatures:, tBat={}, tProp={}, tMain={}, tBPanel={}, tDPanel={}".format(tBat, tProp, tMain, tBPanel, tDPanel))
