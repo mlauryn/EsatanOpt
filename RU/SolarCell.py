3b8c7db63b411fc8958c7e463717974567f61485
Laurynas Maciulis <laurynas.maciulis@vgtu.lt>
2020-03-09 14:34:53 +0200

    SolarCell model updated from paper

--- RU/SolarCell.py
+++ RU/SolarCell.py
@@ -9,37 +9,35 @@ class SolarCell(om.ExplicitComponent):
         self.add_output('eff', val=1.)
         self.declare_partials('*', '*')
     def compute(self, inputs, outputs):
-        """solar cell data from: http://www.azurspace.com/images/0005979-01-00_DB_4G32C_Advanced.pdf"""
-        T0 = 25. #reference temperature
-        eff0 = .318 #efficiency at ref temp
-        Vmp = 3025. # Vmp in mV at ref temp
-        Imp = 433.5 # Imp in mA at ref temp
-        #cell temperature gradients
-        dVmp_dT = -8.6
-        dImp_dT = 0.03
-        deff_dT = (dVmp_dT*Imp + dImp_dT*Vmp)/(Vmp*Imp)
+        """solar cell data from:https://www.e3s-conferences.org/articles/e3sconf/pdf/2017/04/e3sconf_espc2017_03011.pdf"""
+        T0 = 28. #reference temperature
+        eff0 = .285 #efficiency at ref temp
+        T1 = -150.
+        eff1 = 0.335
+
         delta_T = (inputs['tBPanel']+inputs['tDPanel'])/2-T0 #taken as average of both arrays
-        outputs['eff'] = eff0 * (1 + deff_dT * delta_T)
+
+        deff_dT = (eff1 - eff0) / (T1 - T0)
+        
+        outputs['eff'] = eff0 + deff_dT * delta_T
 
     def compute_partials(self, inputs, partials):
         
-        eff0 = .318 #efficiency at ref temp
-        Vmp = 3025. # Vmp in mV at ref temp
-        Imp = 433.5 # Imp in mA at ref temp
-        #cell temperature gradients
-        dVmp_dT = -8.6
-        dImp_dT = 0.03
-        deff_dT = (dVmp_dT*Imp + dImp_dT*Vmp)/(Vmp*Imp)
+        T0 = 28. #reference temperature
+        eff0 = .285 #efficiency at ref temp
+        T1 = -150.
+        eff1 = 0.335
+        deff_dT = (eff1 - eff0) / (T1 - T0)
         
-        partials['eff', 'tBPanel'] = eff0 * deff_dT * 0.5
-        partials['eff', 'tDPanel'] = eff0 * deff_dT * 0.5
+        partials['eff', 'tBPanel'] = deff_dT * 0.5
+        partials['eff', 'tDPanel'] = deff_dT * 0.5
 
 if __name__ == "__main__":
     prob = om.Problem()
     model = prob.model
     indeps = model.add_subsystem('indeps', om.IndepVarComp(), promotes=['*'])
-    indeps.add_output('tBPanel', val=25)
-    indeps.add_output('tDPanel', val=25)
+    indeps.add_output('tBPanel', val=100)
+    indeps.add_output('tDPanel', val=100)
 
     model.add_subsystem('sc', SolarCell(), promotes=['*'])
 
